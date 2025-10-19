d6866cf399c9d13ef80d5d1561977f2bf7c42fc6
Bruno Rehling <brunorehling91@gmail.com>
2025-10-19 18:19:38 -0300

    adjusting buttom colors

--- src/Cadastro.tsx
+++ src/Cadastro.tsx
@@ -107,7 +107,7 @@ function Cadastro() {
                                     </div>
                                 </div>
                             </div>
-                            <button type="submit" className="w-full text-white bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                            <button type="submit" className="w-full text-white !bg-orange-600 !hover:bg-orange-700 focus:ring-4 focus:outline-none !focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                                 Cadastrar
                             </button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
