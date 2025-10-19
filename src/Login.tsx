241f37158dd787dfd0d185941e57ed7cb55554a3
Bruno Rehling <brunorehling91@gmail.com>
2025-10-19 18:34:22 -0300

    adjusting buttom colors

--- src/Login.tsx
+++ src/Login.tsx
@@ -91,7 +91,7 @@ export default function Login() {
           </div>
           <a href="#" className="text-sm font-medium text-orange-600 hover:underline">Esqueceu sua senha?</a>
         </div>
-        <button type="submit" className="w-full text-white !bg-orange-600 !hover:bg-orange-700 !focus:ring-4 !focus:outline-none !focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+        <button type="submit" style={{ backgroundColor: "#ea580c", color: "white" }} className="w-full text-white !bg-orange-600 !hover:bg-orange-700 !focus:ring-4 !focus:outline-none !focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
           Entrar
         </button>
         <p className="text-sm font-light text-gray-500">
