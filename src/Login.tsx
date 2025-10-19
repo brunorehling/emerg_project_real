56203223e363ccfc3da42d519dbe8fac2ebe8345
Bruno Rehling <brunorehling91@gmail.com>
2025-10-19 18:30:37 -0300

    adjusting buttom colors

--- src/Login.tsx
+++ src/Login.tsx
@@ -48,7 +48,7 @@ export default function Login() {
     }
 
     return (
-        <section className="min-h-screen flex items-center justify-center bg-white">
+        <section className="min-h-screen flex items-center justify-center">
   <div className="w-full max-w-md bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
       <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
